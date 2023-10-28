099d9f14c40c8f26412aaf40d3f0137f63409382
hozoo <143112689+hozoo@users.noreply.github.com>
2023-10-28 07:15:18 +0700

    Bew
    Coba

---
+++ KANDURA-KUOTA-TRI.php
@@ -0,0 +1,255 @@
+<?php
+/*
+Provider Tri
+*/
+echo @color("green","     
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWKMMMMMMMMNXNWMMMWNKOxxxkOO0NWWMWWNNNMWKO0KXNWMMMW0OWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWXWMMMMWNNNNNNN0OxddxxdddkOOOKNNWWNXXNNWMMWMMMMWNXkXWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWXMMNNNNNNNNXKkOOOOOOOOOkkkkkKWNK0kO0KXNWMMMMWNXK0WWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWKNNNKXWWXXNKkOOOOOO0OkkkxxxxkNWMXkkkOO0XNWMMWNXKKWWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWNK0OKWKxxkNXNN0OOkkkkkOO0xkkkkxd0WWWM0xdkkO0KXNMMWNKkNWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWNXXKK0NNOxxxxkWWKXNKkkkkkkO0kkkxxxxkNWMMWkxdxkO00NNMMWXox0NWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWXXWWWNX0NkxxxxxONXXXMW0OkkkOO0Okkkkx0XK0XNOxxxxxxk00NXMM0dkkkKWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWKMMMWW0WOkkkkxxkOXKNMMMNXKNW00OOOXNNNMWNxkxxxxxxxkk0KXNNdkkkOOKWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWXMMMKNXkkkkkkkxkOOKWMMMMWMWNKk0OWMMMXXKxxxxxxkxxxkk000ddkkO0XW0WWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWXWMOW0OOOOkkxk0O0kOWMMMMMMNNkk00WMNOkkxxxxxxkkxxkkk0xdkOO0NWWKWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWNOXXK0OOOOOxO0xKkOKWWWWNWNWOkkKKKOkkkkxdkkkkkxkxkkOdoOOKNWMKNWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWkNNX000OOdOKkdKkOOOOO0OKXWKOxkKOOOOkkkdxkkkkxkxOkkdokXWWMKNWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWkNNX0XK0xdKk0O0xkOOOOOOdxXXONxkK0000OkxxOOOOkkkkOxdo0MMMXNWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWkMNKXXKOok0KWWOOokO0000xl0XOWNxdKKKKKddx0000OkOkOxdo0MMNXWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWNNMNKNN0dd00WMMKKdxO0KKKOldK00K0koOXXXxodK0000k0k0ddo0MNXWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWKMMNKXXOldxKKKKKOOk0O0XXXxckXXWMMKdd0XkooK0000k0kkddd0NKWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWW0MMNKXXkcdXWMMMMW0kOWK0XNXodOXWMWWW0dkOxlK0O00k0kdddo0dNWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWW0NMNKXXxl0WMWWWWWWXOXMWKKXXkOk000OOO0xddoK0O0OOOddxxlox0WWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWKKMWKXXkxWW0xdkk00XWXXWMMNNN0Kxkl,''...':dOO0k0Ox00K0ddxWWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWK0WW0XX0do,';.;o:cOWMMWWMMMWN0dlc:'ok'.cl.,xxx0x0OOONxooNWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWXKKWKXX0:,dOd,,:;.:WMMMMMMMMMWXNc,.,,.'ok,lxOOOkOO0XKxod0WWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWN0WKXKXXkoXWXc;:d;:NMMMMMMMMMMMWKlkKKd:kkxxO0OOk0OX0dxodxWWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWNXNXXXXKOKNNxoOXKXWWWMMMMMMMMWWWK0KXKKOKkx000OkK0xxdxodkXWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWXWNXNXX0KNNNNNWWWWWWMMMMMMMMWWWWNNNNOXNkkK00xxxxxxdxdxXXWWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWNWXNXKO0NNNNWWWWWWMMMNWMMMMWWWWWWWXXNWkKKO0dxxxxxdxxOXXNWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWKXNXkx0WWWWWWWWWMMMWMMMMMWWWWWWNNWWN00k00oxxxxxdxO0KWKWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWKKNN0od0NWWWWWMMMWWMMMMMWWWWWWWWWWNKOx0NxoxxxxxdkKOKWKWWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWXXKWNOkk0KWMMMMMWWWWWWWWWNWMMMWWXXOdkKN0doxxxxxd0KO0WXNWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWKKXWNOWKKKNWWMMMMMMMMMMMMMMWWNNKKxkXNX0odxxxxxxKKO0XWKWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNKXXWNKWWXKXXWMMMMMMMMMMMMWNXK0k00XXNKkddxxxxxOKK0OXWKWWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNKXKNXXWWWWNKK00NWMMMMWXK0O000kXXNWXO0dxxxxxxKKK0OXWXNWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNNXKKXWWWWWWNxxkO00OkO0000OO0WWWN0NkdxxxxOkKKK0OWXWKWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWXK0XWWWWXk000000000OOOO0kWWNNWXxxxxxOOkKKK0OWXWKWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWNX0ONXKKNWk000000000000000xKWWWWOxxxxOOO0KKKO0WXWKWWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWWNkollcccl:;,kXNNXOk00000000000000KXXNXXNWxxxkOOO0XKKKOKWXWXNWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWWWWWWk'''''',,::;'XWMMWNNXKK0000000KKXNWMMWWWNKOkxdlokOOK0KOKWKNNXWWWWWWWWWWWWW
+NWXWWWWWWWWWWWWWWWWWWWWWWWd..''';;;;;:;,OXWMMMMMMWWWWWWWWWMMMMMMMWWMMWW0;,;:;,;;;;clxXXXWWWWWWWWWWWWW
+WWXWWWWWWWWWWWWWWWWWWWWWWd'.'';;;:::::::xWNWWMMMMMMMMMMMMMMMMMMMMMWWWXd:::;;;::::::;,:KNWWWWWWWWWWWWW
+WXWWWWWWWWWWWWWWWWWWWWWWx''.,;:;,,;:;;:::XMWWNNWMMMMMMMWNNNNNNNXXXKOl:::;;:;;;;::::,,.:NWWWWWWWWWWWWW
+KWWWWWWWWWWWWWWWWWWWWWWO''..';;'',;::;;::cKMMMXNWMMMWNKXWMMMMMWWXOo::;;;::;;,,,;;;;,'.'OWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWWK'''..,;'',;::::;;;::dNWWKXMMMMWWWMMMWKOdl::;;:::;;;;,',,;;;;,'''lWWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWN,'''..;',;::::::::;;:::dXMWMMMMWX0kdl::;;:;;;::::;,,,'';;;;,,','',WWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWWc''''''';::ldc::::;;,,;;;;lddolc:::;;;;;;;;;::::::;;','.;:;,,,,;,''WWWWWWWWWWWWW
+WWWWWWWWWWWWWWWWWWWWx'''',,;:::OMMo:::;;;;,,'''':;,,,,''''',,,,;;:::::::;,''.;;,','::,'.XWWWWWWWWWWWW
+WNNNNNWWWWWWWWWWWWWo'''',;::::KMMO;::;;;,''''.'dxo.'''',,;;;;;:::;::;;::;;''..'''';::,'.xWWWWWWWWWWWW
+WMMWNNXWWWWWWWWWWNc.'',::::::ONMN:::::::::;;;,0NNk;;;;:::::::::xKWMWWNK0ko,...''',:::;'.;WWWWWWWWWWWW
+NXNNWWWWWWWWWWWWN:.'.,::::::dXKXl:::;;:::::::dNWMc::::::::::;d0occccodk0XNXo..''';:::;'.'OWWWWWWWWWWW
+NWWWWWWWWWWWWWWX;''.';:::::lWMMNkkkxdooooooldXNXO;;;;;:::::;kk::::::::;::;:;...';::::;'.':WWWWWWWWWWW
+WWWWWWWWWWWWWWN;''..,::::;;0MMWXXXWWNNNXKXWWWNNX;;;;;::::::ck;::::::::::::::,..';:::::,'''KWWWWWWWWWW
+KXXXXXXXXXXXXKl......,;;,,,KX0OKKOXXXKK0xldkOOOd',,,,,,,,,;c,;,,,,,,,,,,,,,,'...',,,;,'...lXXXXXXXXXX
+\n");
+echo @color("white","\n");
+echo @color('purple', "Masukkan Nomor Tri \t\t: ");
+$nomor = trim(fgets(STDIN));
+$login = login($nomor);
+echo @color('green', $login['message']."\n");
+echo @color('purple', "Masukkan Kode OTP\t\t: ");
+$otp = trim(fgets(STDIN));
+$login = otplogin($nomor,$otp);
+if (strpos(json_encode($login), '"status":true')) {
+	$secret = $login['secretKey'];
+	$plan = $login['callPlan'];
+    $nomor = $login['msisdn'];
+    $profil = profil($nomor,$plan,$secret);
+    $balance = $profil['creditBalance'];
+    $aktif = $profil['activeUntil'];
+    $sisakuota = $profil['sumOfInternet'];
+    $poin = $profil['stotalPoin'];
+	echo @color('yellow', "\nPulsa Anda\t\t: ");
+	echo @color('nevy', "$balance\n");
+	echo @color('yellow', "Masa Aktif\t\t: ");
+	echo @color('nevy', "$aktif\n");
+    echo @color('yellow', "Sisa Kuota\t\t: ");
+    echo @color('nevy', "$sisakuota\n");
+    echo @color('yellow', "Poin Bonstri\t\t: ");
+    echo @color('nevy', "$poin Poin\n\n");
+    cek:
+    echo @color('green', "Pilih Paket:\n");
+    echo @color('yellow', "1. Welcome Reward 5GB ==> Rp 1\n2. (NEW) 10GB 30 Hari ==> Rp 15000\n3. (NEW) 15GB 30 Hari ==> Rp 20000\n4. 25GB 25rb (Diskon) ==> Rp 25000\n5. 25GB 24 Jam 20 Hari ==> Rp 25000\n6. (NEW) 25GB 20 Hari ==> Rp 25000\n7. (NEW) 25GB 20 Hari ==> Rp 25000\n8. 25GB 24 Jam 30 Hari ==> Rp 29000\n9. (NEW) 25GB 30 Hari ==> Rp 29000\n10. (NEW) 55GB 30 Hari ==> Rp 50000\n11. (NEW) 65GB 30 Hari ==> Rp 60000\n12. (NEW) 75GB 30 Hari ==> Rp 75000\n13. (NEW) 90GB 30 Hari ==> Rp 90000\n14. (NEW) 100GB 30 Hari ==> Rp 90000\n");
+    echo @color('green', "\nMasukkan Pilihan (Sesuai Angka) : ");
+    $pilih = trim(fgets(STDIN));
+    switch ($pilih) {
+            case '1':
+            $prodid = '25669';
+            break;
+            case '2':
+            $prodid = '25245';
+            break;
+            case '3':
+            $prodid = '25459';
+            break;
+            case '4':
+            $prodid = '22648';
+            break;
+            case '5':
+            $prodid = '23160';
+            break;
+            case '6':
+            $prodid = '25254';
+            break;
+            case '7':
+            $prodid = '25264';
+            break;
+            case '8':
+            $prodid = '23164';
+            break;
+            case '9':
+            $prodid = '25267';
+            break;
+            case '10':
+            $prodid = '25469';
+            break;
+            case '11':
+            $prodid = '25690';
+            break;
+            case '12':
+            $prodid = '25247';
+            break;
+            case '13':
+            $prodid = '25476';
+            break;
+            case '14':
+            $prodid = '25693';
+            break;
+        
+        default:
+            echo @color('red', "Pilih Paket Terlebih Dahulu\n\n");
+            goto cek;
+            break;
+    }
+    $cek = cek($prodid);
+    $name = $cek['product']['productName'];
+    $price = $cek['product']['productPrice'];
+    $deskripsi = $cek['product']['productDescription'];
+    echo @color('yellow', "Nama Paket\t: ");
+    echo @color('nevy', "$name\n");
+    echo @color('yellow', "Harga\t\t: ");
+    echo @color('nevy', "$price\n");
+    echo @color('yellow', "Deskripsi\t: ");
+    echo @color('nevy', "\n$deskripsi\n");
+    echo @color('green', "Konfirmasi? (y/n) :");
+    $aa = trim(fgets(STDIN));
+    if(strtolower($aa) !== 'y') {
+        goto cek;
+    }
+    $beli = beli($nomor,$plan,$secret,$prodid);
+    if ($beli['status'] == true) {
+        echo @color('green', "Sukses \n");
+    } else {
+        echo @color('red', "Gagal! \n");
+    }
+
+
+} else {
+    echo @color('red', $login['message']."\n");
+    
+}
+
+function login($nomor){
+	$host = "bimaplus.tri.co.id";        
+    $data = '{"imei":"Android 93488a982824b403","language":1,"msisdn":"'.$nomor.'"}';
+    $ceknom = rekuest($host,"POST",'/api/v1/login/otp-request', $data);
+        return $ceknom;
+}
+function otplogin($nomor,$otp){
+	$host = "bimaplus.tri.co.id";        
+    $data = '{"deviceManufactur":"Samsung","deviceModel":"SMG991B","deviceOs":"Android","imei":"Android 93488a982824b403","msisdn":"'.$nomor.'","otp":"'.$otp.'"}';
+    $ceknom = rekuest($host,"POST",'/api/v1/login/login-with-otp', $data);
+        return $ceknom;
+}
+function profil($nomor,$plan,$secret){
+    $host = "bimaplus.tri.co.id";        
+    $data = '{"callPlan":"'.$plan.'","deviceManufactur":"Samsung","deviceModel":"SMG991B","deviceOs":"Android","imei":"Android 93488a982824b403","language":0,"msisdn":"'.$nomor.'","page":1,"secretKey":"'.$secret.'","subscriberType":"Prepaid"}';
+    $ceknom = rekuest($host,"POST",'/api/v1/homescreen/profile', $data);
+        return $ceknom;
+}
+
+function cek($prodid){
+	$host = "my.tri.co.id";        
+    $data = '{"imei":"WebSelfcare","language":"","callPlan":"","msisdn":"","secretKey":"","subscriberType":"","productId":"'.$prodid.'"}';
+    $ceknom = rekuest($host,"POST",'/apibima/product/product-detail', $data);
+        return $ceknom;
+}
+
+function beli($nomor,$plan,$secret,$prodid){
+    $host = "bimaplus.tri.co.id";        
+    $data = '{"addonMenuCategory":"","addonMenuSubCategory":"","balance":"","callPlan":"'.$plan.'","deviceManufactur":"Samsung","deviceModel":"SMG991B","deviceOs":"Android","imei":"Android 93488a982824b403","language":0,"menuCategory":"3","menuCategoryName":"TriProduct","menuIdSource":"","menuSubCategory":"","menuSubCategoryName":"","msisdn":"'.$nomor.'","paymentMethod":"00","productAddOnId":"","productId":"'.$prodid.'","secretKey":"'.$secret.'","servicePlan":"Default","sms":true,"subscriberType":"Prepaid","totalProductPrice":"","utm":"","utmCampaign":"","utmContent":"","utmMedium":"","utmSource":"","utmTerm":"","vendorId":"11"}';
+    $ceknom = rekuest($host,"POST",'/api/v1/purchase/purchase-product', $data);
+        return $ceknom;
+}
+
+function rekuest($host, $method, $url, $data = null){ 
+        $headers[] = 'Host: '.$host;
+		$headers[] = 'App-Version: 4.2.6';
+        $headers[] = 'Content-Type: application/json; charset=UTF-8';
+        $headers[] = 'User-Agent: okhttp/4.9.0';
+        
+        $c = curl_init("https://".$host.$url);  
+        switch ($method){
+            case "GET":
+            curl_setopt($c, CURLOPT_POST, false);
+            break;
+            case "POST":               
+            curl_setopt($c, CURLOPT_POST, true);
+            curl_setopt($c, CURLOPT_POSTFIELDS, $data);
+            break;
+            case "PUT":               
+            curl_setopt($c, CURLOPT_CUSTOMREQUEST, "PUT");
+            curl_setopt($c, CURLOPT_POSTFIELDS, $data);
+            break;
+        }
+        
+        curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($c, CURLOPT_HEADER, true);
+        curl_setopt($c, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 20);
+        curl_setopt($c, CURLOPT_TIMEOUT, 20);
+        $response = curl_exec($c);
+        $httpcode = curl_getinfo($c);
+        if (!$httpcode){
+            return false;
+        }
+        else {
+            $headers = substr($response, 0, curl_getinfo($c, CURLINFO_HEADER_SIZE));
+            $body   = substr($response, curl_getinfo($c, CURLINFO_HEADER_SIZE));
+        }
+        
+        curl_close($c);
+        $json = json_decode($body, true);
+        return $json;
+    }
+
+
+function color($color = "default" , $text = "")
+    {
+        $arrayColor = array(
+            'grey'      => '1;30',
+            'red'       => '1;31',
+            'green'     => '1;32',
+            'yellow'    => '1;33',
+            'blue'      => '1;34',
+            'purple'    => '1;35',
+            'nevy'      => '1;36',
+            'white'     => '1;0',
+        );  
+        return "\033[".$arrayColor[$color]."m".$text."\033[0m";
+    }
